c9d8d2d33b88c8b7b11ec01ba15d86f5a85e509a
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-01-17 19:21:14 -0300

    Movie Cards Library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
+      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa. */}
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪
     </div>
   );
