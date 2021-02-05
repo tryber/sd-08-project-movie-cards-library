ed0b5790ecf074a79671575db8bd990599d01bef
Víctor Cançado <victor.cancado@gmail.com>
2021-02-05 03:44:42 -0300

    Requisito 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+//   constructor(movies) {
+//     super(movies);
+//   }
+
+  render() {
+    return (
+      <MovieList>
+        <MovieCard />
+      </MovieList>
+    );
+  }
+}
+
+export default MovieList;
